bd33079354639aa7b1f6bbec33510910776bdb20
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-08-07 16:36:40 +0530

    updated problem statement

--- src/edu/psanglikar/codechef/beginner/Test.java
+++ src/edu/psanglikar/codechef/beginner/Test.java
@@ -4,8 +4,27 @@
 package edu.psanglikar.codechef.beginner;
 
 /**
+ * <pre>
+ * Your program is to use the brute-force approach in order to 
+ * find the Answer to Life, the Universe, and Everything. 
+ * More precisely... 
+ * rewrite small numbers from input to output. 
+ * Stop processing input after reading in the number 42.
+ * All numbers at input are integers of one or two digits.
+ * 
+ * Example Input:
+ * 1
+ * 2
+ * 88
+ * 42
+ * 99
+ * 
+ * Output:
+ * 1
+ * 2
+ * 88
  * @author Pratik Sanglikar
- *
+ * </pre>
  */
 public class Test {
 	public static void main(String args[]) {
