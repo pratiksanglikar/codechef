7fb36bf4413fc4b558d4626ab897ec4c5474d5c4
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-08-10 17:36:55 +0530

    Codechef START01

---
+++ src/edu/psanglikar/codechef/beginner/START01.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.psanglikar.codechef.beginner;
+
+import java.util.Scanner;
+
+/**
+ * The solution for the problem - 
+ * <a href="https://www.codechef.com/problems/START01">START01</a>
+ * 
+ * @author Pratik Sanglikar
+ */
+public class START01 {
+
+	private static Scanner scanner = new Scanner(System.in);
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(scanner.nextInt());
+	}
+}
