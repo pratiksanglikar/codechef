38714571accf3cd5b1444f1bb76cd9f173fa35d1
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-09-11 21:34:10 -0700

    Karumanchi Doubly Linked List insertion

--- src/edu/pratiksanglikar/karumanchi/linkedlists/DoublyLinkedList.java
+++ src/edu/pratiksanglikar/karumanchi/linkedlists/DoublyLinkedList.java
@@ -103,4 +103,111 @@ public class DoublyLinkedList {
 			this.data = data;
 		}
 	}
+	
+	private DLLNode head = null;
+	private DLLNode tail = null;
+	private int length = 0;
+	
+	/**
+	 * initialize empty Doubly Linked List.
+	 */
+	public DoublyLinkedList() {
+		this.tail = null;
+		this.head = null;
+		this.length = 0;
+	}
+	
+	/**
+	 * initialize the Doubly Linked List with head node.
+	 * @param head
+	 */
+	public DoublyLinkedList(DLLNode head) {
+		this.head = head;
+		this.tail = null;
+		this.length = 1;
+	}
+
+	/**
+	 * returns the head of the Doubly Linked List.
+	 * @return the head
+	 */
+	public DLLNode getHead() {
+		return head;
+	}
+
+	/**
+	 * returns the length of the Doubly Linked List.
+	 * @return the length
+	 */
+	public int getLength() {
+		return length;
+	}
+	
+	/**
+	 * returns the tail of the Doubly Linked List.
+	 * @return the tail of the linked List
+	 */
+	public DLLNode getTail() {
+		return tail;
+	}
+
+	/**
+	 * sets the tail of the DLL.
+	 * @param tail the tail to set
+	 */
+	public void setTail(DLLNode tail) {
+		this.tail = tail;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		if(head == null) {
+			return "";
+		}
+		sb.append(head.getData());
+		for(DLLNode iterator = head.getNext(); iterator != null; iterator = iterator.getNext()) {
+			sb.append("<==>" + iterator.getData());
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * inserts a DLLNode at the beginning of the Doubly Linked List.
+	 * 
+	 * @param node to be inserted in DLL
+	 */
+	public void insertAtBeginning(DLLNode node) {
+		if(head == null) {
+			head = node;
+			tail = node;
+			length++;
+			return;
+		}
+		node.setNext(head);
+		head.setPrev(node);
+		head = node;
+		length++;
+	}
+	
+	/**
+	 * inserts a node at the end of the Doubly Linked List
+	 * 
+	 * @param node to be inserted in DLL.
+	 */
+	public void insertAtTheEnd(DLLNode node) {
+		if(head == null) {
+			head = node;
+			tail = node;
+			length++;
+			return;
+		}
+		tail.setNext(node);
+		node.setPrev(tail);
+		tail = node;
+		length++;
+	}
 }
--- test/edu/pratiksanglikar/karumanchi/linkedlists/DoublyLinkedListTest.java
+++ test/edu/pratiksanglikar/karumanchi/linkedlists/DoublyLinkedListTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.pratiksanglikar.karumanchi.linkedlists.DoublyLinkedList.DLLNode;
+
 /**
  * This class is for testing purposes of Doubly Linked List and 
  * operations on it.
@@ -16,7 +18,39 @@ import org.junit.Test;
 public class DoublyLinkedListTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void insertionAtBeginningTest() {
+		insertAtBeginningEmptyListTest();
+		insertAtBeginningNonEmptyListTest();
+	}
+
+	private void insertAtBeginningNonEmptyListTest() {
+		DoublyLinkedList dl = new DoublyLinkedList(new DLLNode(34));
+		dl.insertAtBeginning(new DLLNode(12));
+		assertEquals("It should add a node to a non empty list at beginning of the list", 12, dl.getHead().getData());
+	}
+
+	private void insertAtBeginningEmptyListTest() {
+		DoublyLinkedList dl = new DoublyLinkedList(new DLLNode(34));
+		assertEquals("It should add a node to a non empty list at beginning of the list", 34, dl.getHead().getData());
+	}
+	
+	@Test
+	public void insertAtTheEndTest() {
+		insertAtTheEndEmptyListTest();
+		insertAtTheEndNonEmptyListTest();
+	}
+
+	private void insertAtTheEndNonEmptyListTest() {
+		DoublyLinkedList dl = new DoublyLinkedList(new DLLNode(12));
+		dl.insertAtBeginning(new DLLNode(10));
+		dl.insertAtBeginning(new DLLNode(5));
+		dl.insertAtTheEnd(new DLLNode(99));
+		assertEquals("It should add a node to the end of non empty list ", "5<==>10<==>12<==>99", dl.toString());
+	}
+
+	private void insertAtTheEndEmptyListTest() {
+		DoublyLinkedList dl = new DoublyLinkedList();
+		dl.insertAtTheEnd(new DLLNode(55));
+		assertEquals("It should add a node to the end of an empty list ", 55, dl.getHead().getData());
 	}
 }
